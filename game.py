76589116d9db5d8e035e6798a870515de23a5f94
Taylor <takeller17@gmail.com>
2022-10-13 08:58:51 -0600

    Refactors play loop and updates __init__ to use goal setter

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ from player import Player
 
 
 class PlayGame:
-    def __init__(self, name1, name2, goal, dice) -> None:
+    def __init__(self, name1: str, name2: str, goal: int, dice: int) -> None:
         self.player1 = Player(name1)
         self.player2 = Player(name2)
-        self._goal = goal
+        self.goal = goal
         self.dice = Dice(dice)
 
     @property
@@ -27,20 +27,32 @@ class PlayGame:
             p1_roll = self.dice.roll()
             p2_roll = self.dice.roll()
 
+
             if p1_roll == p2_roll:
                 continue
             elif p1_roll > p2_roll:
                 self.player1.add_points(p1_roll)
-                if self.dice.is_max_roll(p1_roll):
-                    self.player2.lose_points(p2_roll)
             else:
                 self.player2.add_points(p2_roll)
-                if self.dice.is_max_roll(p2_roll):
-                    self.player1.lose_points(p1_roll)
 
-            if self.player1.score > self.goal:
-                print(f"{self.player1.name}: {self.player1.score}")
+            if self.dice.is_max_roll(p2_roll):
+                self.player1.lose_points(p1_roll)
+
+            if self.dice.is_max_roll(p1_roll):
+                self.player2.lose_points(p2_roll)
+
+            if self.is_winner(self.player1):
+                self.print_win_message(self.player1)
                 break
-            elif self.player2.score > self.goal:
-                print(f"{self.player2.name}: {self.player2.score}")
+            elif self.is_winner(self.player2):
+                self.print_win_message(self.player2)
                 break
+
+    def is_winner(self, player: Player) -> bool:
+        if player.score >= self.goal:
+            return True
+
+        return False
+
+    def print_win_message(self, player: Player) -> None:
+        print(f"{player.name}: {player.score}")
